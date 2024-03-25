8f039aab4eb3addba319821a4bab16961ca13399
unknown <tiltiol@knu.ua>
2024-03-25 11:19:07 +0200

    Refactored

--- Helpers/helpers.cpp
+++ Helpers/helpers.cpp
@@ -52,49 +52,6 @@ void getMaster(uint32_t studioID, std::string clarifier) {
     }
 }
 
-//void getSlave(uint32_t filmId, uint32_t studioId, std::string clarifier) {
-//    Studio studio = getStudio(studioId);
-//
-//    if (studio.getStudioId() == 0) {
-//        std::cout << "Studio with ID " << studioId << " not found." << std::endl;
-//        return;
-//    }
-//
-//    int64_t firstStudiosFilmAddress = studio.getFirstStudiosFilmAddress();
-//
-//    Film film;
-//    bool found = false;
-//    FILM_FILE.seekg(firstStudiosFilmAddress * sizeof(Film));
-//    while (FILM_FILE.read(reinterpret_cast<char*>(&film), sizeof(Film))) {
-//        if (film.getFilmId() == filmId) {
-//            found = true;
-//            break;
-//        }
-//    }
-//
-//    if (!found) {
-//        std::cout << "Film with ID " << filmId << " not found for Studio " << studioId << "." << std::endl;
-//        return;
-//    }
-//
-//    //FILM_FILE.seekg(firstStudiosFilmAddress * sizeof(Film));
-//    //FILM_FILE.read(reinterpret_cast<char*>(&film), sizeof(Film));
-//
-//    if (clarifier == "full") {
-//        std::cout << "Name: " << film.getFilmName() << std::endl;
-//        std::cout << "Studio ID: " << film.getStudioId() << std::endl;
-//        std::cout << "Budget: " << film.getBudget() << std::endl;
-//    } else if (clarifier == "studioName") {
-//        std::cout << "Name: " << film.getFilmName() << std::endl;
-//    } else if (clarifier == "studioId") {
-//        std::cout << "Studio ID: " << film.getStudioId() << std::endl;
-//    } else if (clarifier == "budget") {
-//        std::cout << "Budget: " << film.getBudget() << std::endl;
-//    } else {
-//        std::cout << "Invalid clarifier: " << clarifier << std::endl;
-//    }
-//}
-
 void getSlave(uint32_t filmId, uint32_t studioId, std::string clarifier) {
     Film film = getFilm(filmId, studioId);
 
@@ -113,30 +70,10 @@ void getSlave(uint32_t filmId, uint32_t studioId, std::string clarifier) {
     }
 }
 
-//bool updateStudio(uint32_t studioId, const char* newName) {
-//    // Find the studio record
-//    Studio studio = getStudio(studioId);
-//    if (studio.getStudioId() == 0) {
-//        // Studio not found
-//        return false;
-//    }
-//
-//    // Update the studio studioName
-//    studio.setStudioName(newName);
-//
-//    // Update the studio record in the file
-//    uint32_t studioAddress = studio.getNextStudioAddress();
-//    STUDIO_FILE.seekp(sizeof(Studio) * studioAddress);
-//    STUDIO_FILE.write(reinterpret_cast<const char*>(&studio), sizeof(Studio));
-//    STUDIO_FILE.flush();
-//
-//    return !STUDIO_FILE.fail();
-//}
-
-bool delFilm(uint32_t id, uint32_t studioId) {//тяжко
+bool delFilm(uint32_t id, uint32_t studioId) {
     Film film = getFilm(id, studioId);
 
-    uint32_t next = film.getNext();
+    /**/
 
-    return 0;
+    return false;
 }
