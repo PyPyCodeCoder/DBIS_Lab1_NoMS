f735db98a7d465cd4227ef7f83fe86ade8049df0
unknown <tiltiol@knu.ua>
2024-03-26 03:29:57 +0200

    Final commit

--- main.cpp
+++ main.cpp
@@ -28,12 +28,58 @@ int main() {
 
             //perform commands actions
             if (command.at(0) == "insert-m") {
-                Studio studio(std::stoul(command.at(1)), command.at(2).c_str());
-                studio.insert();
+                bool found = false;
+                IND_FILE.seekg(0, std::ios::beg);
+                std::pair<uint32_t, uint32_t> tempPair;
+                while (IND_FILE.read(reinterpret_cast<char*>(&tempPair), sizeof(tempPair))) {
+                    if (tempPair.first == std::stoul(command.at(1))) {
+                        found = true;
+                    }
+                }
+
+                IND_FILE.clear();
+                IND_FILE.seekg(0, std::ios::beg);
+
+                if(found) {
+                    std::cout << "Studio with ID " << std::stoul(command.at(1)) << " already exists." << std::endl;
+                }
+                else {
+                    Studio studio(std::stoul(command.at(1)), command.at(2).c_str());
+                    studio.insert();
+                }
             }
             else if (command.at(0) == "insert-s") {
-                Film film(std::stoul(command.at(1)), std::stoul(command.at(2)), command.at(3).c_str(), std::stoul(command.at(4)));
-                film.insert();
+                bool found = false;
+                FILM_FILE.seekg(0, std::ios::beg);
+
+                Studio studio = getStudio(std::stoul(command.at(2)));
+                if (studio.getStudioId() == 0) {
+                    found = false;
+                }
+                int64_t firstStudiosFilmAddress = studio.getFirstStudiosFilmAddress();
+                Film film;
+                FILM_FILE.seekg(firstStudiosFilmAddress * sizeof(Film));
+                while (FILM_FILE.read(reinterpret_cast<char*>(&film), sizeof(Film))) {
+                    if (film.getFilmId() == std::stoul(command.at(1))) {
+                        found = true;
+                    }
+                    if (film.getNext() != -1) {
+                        FILM_FILE.seekg(film.getNext() * sizeof(Film));
+                    } else {
+                        break;
+                    }
+                }
+
+                FILM_FILE.clear();
+                FILM_FILE.seekg(0, std::ios::beg);
+
+                if(found) {
+                    std::cout << "Film with ID " << std::stoul(command.at(1)) << " already exists." << std::endl;
+                }
+                else {
+                    Film film(std::stoul(command.at(1)), std::stoul(command.at(2)), command.at(3).c_str(), std::stoul(command.at(4)));
+                    film.insert();
+                }
             }
             else if (command.at(0) == "get-m") {
                 getMaster(std::stoul(command.at(1)), command.at(2));
